6f5da8a0b379c92d0cada8d8328b283b7399ce1e
Aphisit Namracha <aphisiit086757@hotmail.com>
2018-01-04 01:06:11 +0700

    spearead service and repository.

--- src/main/java/com/controller/EmployeeController.java
+++ src/main/java/com/controller/EmployeeController.java
@@ -1,15 +1,15 @@
 package com.controller;
 
 import com.model.Employee;
+import com.service.EmployeeService;
 import flexjson.JSONSerializer;
-import com.repo.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+import javax.annotation.Resource;
 import java.util.logging.Logger;
 
 /**
@@ -21,22 +21,22 @@ public class EmployeeController {
 
     private static Logger logger = Logger.getLogger(EmployeeController.class.getName());
 
-    private final EmployeeRepository employeeRepository;
-
     @Autowired
-    EmployeeController(EmployeeRepository employeeRepository){
-        this.employeeRepository = employeeRepository;
+    private final EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
     }
 
 
     @CrossOrigin("*")
-    @GetMapping("/list")
+    @GetMapping(value = "/list",produces = "text/html; charset=utf-8",headers = "Accept=applicatino/json; charset=utf-8")
     public ResponseEntity<String> getEmployees(){
         HttpHeaders headers = new HttpHeaders();
         headers.add("Content-Type","application/json; charset=utf-8");
         try{
             logger.info("[getEmployee] -> Get data from list");
-            return  new ResponseEntity<String>(new JSONSerializer().exclude("*.class").serialize(employeeRepository.findAll()),headers,HttpStatus.OK);
+            return  new ResponseEntity<String>(new JSONSerializer().exclude("*.class").serialize(employeeService.findAll()),headers,HttpStatus.OK);
         }catch (Exception e) {
             logger.info("[postEmployee] -> Exception >>> " + e.getMessage());
             return new ResponseEntity<String>("{\"ERROR\":" +e.getMessage()+ "\"}", headers, HttpStatus.INTERNAL_SERVER_ERROR);
@@ -44,14 +44,14 @@ public class EmployeeController {
     }
 
     @CrossOrigin("*")
-    @GetMapping("/findById")
+    @GetMapping(value = "/findById/{id}",produces = "text/html; charset=utf-8",headers = "Accept=applicatino/json; charset=utf-8")
     public ResponseEntity<String> findById(@RequestParam("id") Long id){
         logger.info("[getEmployee] -> Get Id");
         HttpHeaders headers = new HttpHeaders();
         headers.add("Content-Type","application/json; charset=utf-8");
         try{
             logger.info("[getEmployee] -> Get Id");
-            return new ResponseEntity<String>((new JSONSerializer().exclude("*.class").serialize(employeeRepository.findById(id))),headers,HttpStatus.OK);
+            return new ResponseEntity<String>((new JSONSerializer().exclude("*.class").serialize(employeeService.findEmployeeById(id))),headers,HttpStatus.OK);
         }catch (Exception e){
             logger.info("[getEmployee] -> " + e.getMessage());
             return new ResponseEntity<String>("{\"ERROR\":" +e.getMessage()+ "\"}", headers, HttpStatus.INTERNAL_SERVER_ERROR);
@@ -59,7 +59,7 @@ public class EmployeeController {
     }
 
     @CrossOrigin("*")
-    @GetMapping("/findByFirstNameAndLastName")
+    @GetMapping(value = "/findByFirstNameAndLastName",produces = "text/html; charset=utf-8",headers = "Accept=applicatino/json; charset=utf-8")
     public ResponseEntity<String> findByFirstNameAndLastName(@RequestParam("firstName") String firstName,@RequestParam("lastName") String lastName){
         logger.info("[findByfirstName] -> Get firstNameAndLastName");
         HttpHeaders headers = new HttpHeaders();
@@ -67,7 +67,7 @@ public class EmployeeController {
         try{
             logger.info("[getEmployee] -> Get Id");
             return new ResponseEntity<String>((new JSONSerializer().exclude("*.class")
-                    .serialize(employeeRepository.findByFirstNameIgnoreCaseContainingAndLastNameIgnoreCaseContaining(firstName,lastName))),headers,HttpStatus.OK);
+                    .serialize(employeeService.findByFistNameAndLastName(firstName,lastName))),headers,HttpStatus.OK);
         }catch (Exception e){
             logger.info("[getEmployee] -> " + e.getMessage());
             return new ResponseEntity<String>("{\"ERROR\":" +e.getMessage()+ "\"}", headers, HttpStatus.INTERNAL_SERVER_ERROR);
@@ -80,12 +80,10 @@ public class EmployeeController {
                                        @RequestParam String lastName,
                                        @RequestParam String description){
         Employee employee = new Employee(firstName,lastName,description);
-        logger.info("[postEmployee] -> dataIN ==> firstName : " + firstName +
-                " ,lastName : " + lastName + " ,description : " + description);
 
         try{
             logger.info("[postEmployee] -> Insert data successful");
-            employeeRepository.save(employee);
+            employeeService.save(employee.toString());
             return new ResponseEntity(HttpStatus.OK);
         }catch (Exception e){
             logger.info("[postEmployee] -> Exception >>> " + e.getMessage());
--- src/main/java/com/service/EmployeeService.java
+++ src/main/java/com/service/EmployeeService.java
@@ -1,8 +1,16 @@
 package com.service;
 
+import com.model.Employee;
+
+import java.util.List;
+
 /**
  * Created by aphisit on 02-Aug-17.
  */
-public class EmployeeService {
+public interface EmployeeService {
+    List<Employee> findAll();
+    Employee findEmployeeById(Long id);
+    void save(String json);
+    List<Employee> findByFistNameAndLastName(String firstName,String lastName);
 
 }
